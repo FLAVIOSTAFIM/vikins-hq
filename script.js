3b3b22fa74744167c7a1e2a3fc0ef6bbc0adf427
flavio <104777358+FLAVIOSTAFIM@users.noreply.github.com>
2024-06-27 15:19:40 -0300

    frffdff

--- script.js
+++ script.js
@@ -9,67 +9,33 @@ const perguntas = [
         enunciado: "reza a lenda que muito tempo atrás, uma viking chamada s/n era a mais forte e habilidosa dentre sua tribo,treinando todos os dias sonhando em conquistar terras e mares,será que ela vai conquistar seus sonhos? ",
         alternativas: [
             {
-                texto: "vocẽ acha importante a s/n continuar treinando",
-                afirmacao: "é necessário para se tornar uma guereira  "
+                texto: "sim ela e uma boa guerreira, e irá criar seu própio batalhão",
+                afirmacao: "sempre treinando nos momentos que pode"
             },
             {
-                texto: "não precisa continuar treinando",
-                afirmacao: "sofrer bulling"
+                texto: "não sua tribo não a apoia",
+                afirmacao: "desistir"
             }
         ]
     },
     {
-        enunciado: "ela criou um bando de guerreiros,então construiram um navio partindo em busca de aventuras para contar para as proximas gerações, navegando por mares tempestuosos enfrentando monstros e deuses ao além do horizonte, e ao chegar em terras desconhecidas, os guerreiros enfrentaram inúmeros desafios e batalhas, lutaram contra inimigos, estabeleceram alianças com ?   ",
+        enunciado: "ela criou um batalhão de guerreiros,e construiram um navio partindo em busca de aventuras e conquistas para contar para as proximas gerações, navegando por mares tempestuosos enfrentando monstros e deuses ao além do horizonte, e ao chegar em terras desconhecidas, os guerreiros enfrentaram inúmeros desafios e batalhas, lutaram contra inimigos, estabeleceram alianças...   ",
         alternativas: [
             {
-                texto: "não estabelecer nenhuma aliança",
-                afirmacao: "ficando com seu bando"
+                texto: "com reis e lords de cada lugar aonde passavam",
+                afirmacao: "ficando com sempre com seus guerreiros"
             },
             {
-                texto: "estabeleçer uma aliança",
+                texto: "não estabeleçer uma aliança",
                 afirmacao: "podendo ajudar no futuro"
             }
         ]
     },
     {
         enunciado: "A s/n acabou se tornando uma lider forte e companheira ajudando e cuidando de seus guerreiros de forma justa e persistente, e durante sua vida precisou fazer escolhas dificeis que testaram sua lealdade ",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
-        ]
-    },
+        
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
-        ]
-    },
+        
 ];
 
 
